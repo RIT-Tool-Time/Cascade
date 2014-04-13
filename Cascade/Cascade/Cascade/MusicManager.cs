bd7b2e4ac8130ccf69ee3ccb78b3f8a2ca63fccf
crb9826 <crb9826@rit.edu>
2014-04-13 17:46:18 -0400

    Got the beat timer working

--- Cascade/Cascade/Cascade/MusicManager.cs
+++ Cascade/Cascade/Cascade/MusicManager.cs
@@ -5,12 +5,14 @@ using System.Text;
 using Microsoft.Xna.Framework.Audio;
 using System.Diagnostics;
 using Microsoft.Xna.Framework;
+using System.Timers;
 
 namespace Cascade
 {
     public enum MusicalScaleType { Major, Minor, Pentatonic }
 
     public enum MusicState { Unknown, WaitingToStartAnalysis, Analysis, Level1, Level2, Level3 }
+    public enum Tempo { T100 = 100, T120 = 120, T140 = 140 }
     public static class MusicManager
     {
         public static readonly int[] MajorScale = { 0, 2, 4, 5, 6, 9, 11, 12 };
@@ -19,13 +21,16 @@ namespace Cascade
 
         public static readonly int[,] Chords = 
         {
+            { 0, 2, 4 },
             { 1, 3, 5 },
             { 2, 4, 6 },
-            { 3, 5, 7 },
+            { 3, 5, 0 },
             { 4, 6, 1 },
-            { 5, 7, 2 },
-            { 6, 1, 3 }
+            { 5, 0, 2 }
         };
+        static int[] chordProgression = null;
+
+        static Tempo tempo = Tempo.T100;
 
         public static List<MusicalNote> SoundEffects = new List<MusicalNote>();
 
@@ -37,6 +42,16 @@ namespace Cascade
         static Stopwatch stopWatch = new Stopwatch();
         static List<NoteTimeInfo> notesPlayed = new List<NoteTimeInfo>();
         static NoteTimeAnalysis[] analysis;
+
+        static int numberOfUnits = 0;
+
+        static int unit = 0;
+        static int bar = 0;
+        static int beat = 0;
+        static int note = 0;
+        static Timer timer =null;
+
+        
         
         public static SoundEffect LoadUserSound(int UserNumber, int MidiNumber, int Version, string InstrumentName)
         {
@@ -96,11 +111,33 @@ namespace Cascade
             Global.Output += "Waiting to start analysis";
             state = MusicState.WaitingToStartAnalysis;
         }
+        public static void CreateChordProgression()
+        {
+            Random rand = new Random();
+            chordProgression = new int[4];
+            int lastVal = 0;
+            chordProgression[0] = 0;
+            for (int i = 1; i < chordProgression.Length; i++)
+            {
+                int val = 0;
+                while (chordProgression.Contains(val))
+                {
+                    val = rand.Next(Chords.GetLength(0));
+                }
+                chordProgression[i] = val;
+            }
+            string s = "";
+            foreach (var c in chordProgression)
+            {
+                s += c + ", ";
+            }
+            Global.Output += "Chord progression: " + s;
+        }
         public static void NotePlayedByUser(int midiNumber, int playerIndex, int instrumentNumber)
         {
             if (state == MusicState.WaitingToStartAnalysis)
             {
-                state = MusicState.Analysis;
+                SetState(MusicState.Analysis);
                 stopWatch.Restart();
                 notesPlayed.Clear();
 
@@ -136,6 +173,11 @@ namespace Cascade
         }
         public static void Update()
         {
+            if (timer == null)
+            {
+                timer = new Timer();
+                timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            }
             foreach (var p in PanelManagers)
             {
                 p.Update();
@@ -146,7 +188,7 @@ namespace Cascade
                 if (SoundEffects[i].State == SoundState.Stopped)
                 {
                     SoundEffects.RemoveAt(i);
-                    Global.Output += "Removed sound";
+                    //Global.Output += "Removed sound";
                     i--;
                 }
             }
@@ -156,18 +198,154 @@ namespace Cascade
                 {
                     stopWatch.Stop();
                     stopWatch.Reset();
-                    state = MusicState.Level1;
                     analysis = new NoteTimeAnalysis[PanelManagers.Count];
                     for (int i = 0; i < PanelManagers.Count; i++)
                     {
                         analysis[i] = NoteTimeAnalysis.Analyze(i, notesPlayed, timeForAnalysis);
                         Global.Output += "Analysis for player " + (i + 1) + " - " + analysis[i];
                     }
-                    
+                    CreateChordProgression();
+                    SetState(MusicState.Level1);
                 }
             }
         }
+
+        static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            note++;
+            NoteIncremented();
+            if (note % 4 == 0)
+            {
+                beat++;
+                BeatIncremented();
+                Global.Output += "Beat " + beat;
+                if (beat % 4 == 0)
+                {
+                    bar++;
+                    BarIncremented();
+                    Global.Output += "Bar " + bar;
+                    if (bar % 4 == 0)
+                    {
+                        unit++;
+                        UnitIncremented();
+                        Global.Output += "Unit " + unit;
+                    }
+                }
+            }
+        }
+        static void NoteIncremented()
+        {
+
+        }
+        static void BeatIncremented()
+        {
+
+        }
+        static void BarIncremented()
+        {
+
+        }
+        static void UnitIncremented()
+        {
+            if (unit >= numberOfUnits)
+            {
+                if (state == MusicState.Level1)
+                {
+                    SetState(MusicState.Level2);
+                }
+                else if (state == MusicState.Level2)
+                {
+                    SetState(MusicState.Level3);
+                }
+            }
+        }
+        public static void SetState(MusicState s)
+        {
+            if (s != state)
+            {
+                Global.Output += "State being set to " + s;
+                switch (s)
+                {
+                        
+                    case MusicState.Analysis:
+
+                        break;
+                    case MusicState.Level1:
+                        if (analysis != null && analysis.Length > 0)
+                        {
+                            float bpm = 0;
+                            for (int i = 0; i < analysis.Length; i++)
+                            {
+                                bpm += analysis[0].BPM;
+                            }
+                            bpm /= analysis.Length;
+                            Global.Output += "Average BPM of all players: " + bpm;
+                            tempo = Tempo.T100;
+                            SetTimer(s, tempo);
+                        }
+                        break;
+                    case MusicState.Level2:
+                        SetTimer(s, tempo);
+                        break;
+                    case MusicState.Level3:
+                        SetTimer(s, tempo);
+                        break;
+                }
+                state = s;
+                
+            }
+        }
+        public static void SetTimer(MusicState s, Tempo t)
+        {
+            Random r = new Random();
+            switch (s)
+            {
+                case MusicState.Level1:
+                    if (t == Tempo.T140)
+                    {
+                        numberOfUnits = 3;
+                    }
+                    else
+                    {
+                        numberOfUnits = 2;
+                    }
+                    break;
+                case MusicState.Level2:
+                    if (t == Tempo.T100)
+                    {
+                        numberOfUnits = r.Next(6, 8);
+                    }
+                    else if (t == Tempo.T120)
+                    {
+                        numberOfUnits = r.Next(7, 10);
+                    }
+                    else if (t == Tempo.T140)
+                    {
+                        numberOfUnits = r.Next(9, 12);
+                    }
+                    break;
+                case MusicState.Level3:
+                    if (t == Tempo.T100)
+                    {
+                        numberOfUnits = r.Next(4, 6);
+                    }
+                    else if (t == Tempo.T120)
+                    {
+                        numberOfUnits = r.Next(4, 7);
+                    }
+                    else if (t == Tempo.T140)
+                    {
+                        numberOfUnits = r.Next(5, 8);
+                    }
+                    break;
+            }
+            note = beat = bar = unit = 0;
+            timer.Interval = (60d / (int)tempo) * 1000d / 4d;
+            timer.Stop();
+            timer.Start();
+        }
     }
+    
     public class MusicalNote
     {
         bool fade = false, fadeIn = true;
@@ -267,7 +445,7 @@ namespace Cascade
                 speed *= -1;
             }
             vol += speed;
-            Global.Output += GetVolume();
+            //Global.Output += GetVolume();
         }
         protected override float GetVolume()
         {
